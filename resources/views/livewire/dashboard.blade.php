269f01e0d2253f0eeae92e9cd35f91e933d97f87
haddadzineddine <hz_haddad@esi.dz>
2022-07-27 23:20:59 +0100

    wip

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -68,12 +68,12 @@
 
     @if ($link || $this->getUserInfo())
         <div class="flex justify-center  mt-24">
-            <div class="flex  p-3 rounded-lg bg-slate-100 justify-between space-x-4  opacity-90">
+            <div class="flex  p-3 rounded-lg bg-slate-100 items-center justify-between space-x-8  opacity-90">
 
-                <div class="text-xs">{{ $link ?? $this->getUserInfo()->link }}http://127.0.0.1:8000/dashboard</div>
+                <div class="text-sm">{{ $link ?? $this->getUserInfo()->link }}http://127.0.0.1:8000/dashboard</div>
 
-                <button wire:click="copyToClipboard" class="flex space-x-2 ">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
+                <button wire:click="copyToClipboard" class="flex space-x-4 items-center justify-center text-sm">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round"
                             d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z" />
